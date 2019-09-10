71411711cc950e8ed846250f421a8b141731c929
Matt <matt.rothwell.m@gmail.com>
2019-09-10 23:05:11 +1200

    Added some method stubs.

---
+++ src/nz/ac/vuw/ecs/swen225/a3/application/Game.java
@@ -0,0 +1,22 @@
+package nz.ac.vuw.ecs.swen225.a3.application;
+
+import nz.ac.vuw.ecs.swen225.a3.maze.Maze;
+import nz.ac.vuw.ecs.swen225.a3.maze.Player;
+
+public class Game {
+
+    private Player player;
+    private Maze maze;
+
+    public void update(){
+
+    }
+
+    public void loadGame(){
+
+    }
+
+    public void saveGame(){
+
+    }
+}
