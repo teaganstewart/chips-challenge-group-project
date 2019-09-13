8e8ce4754be40b88e7974b09ba37caf28820e5cd
David <mengveng17@gmail.com>
2019-09-13 13:51:17 +1200

    Revert "Implements KeyEvent"
    This reverts commit 4a5b36f162bf91c2cb71cb8d3d400fcd29b8f674.

--- src/nz/ac/vuw/ecs/swen225/a3/application/ui/GUI.java
+++ src/nz/ac/vuw/ecs/swen225/a3/application/ui/GUI.java
@@ -2,9 +2,7 @@ package nz.ac.vuw.ecs.swen225.a3.application.ui;
 
 import nz.ac.vuw.ecs.swen225.a3.application.Game;
 
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -14,7 +12,7 @@ public class GUI extends Frame {
 
     public GUI(){
         game = new Game();
-        gameWindow();
+
         this.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
@@ -28,50 +26,13 @@ public class GUI extends Frame {
 
             @Override
             public void keyReleased(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_X && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-                    System.out.println("exit");
-                }
-                if (e.getKeyCode() == KeyEvent.VK_S && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-                    System.out.println("save");
-                }
-                if (e.getKeyCode() == KeyEvent.VK_R && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-                    System.out.println("resume");
-                }
-                if (e.getKeyCode() == KeyEvent.VK_P && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-                    System.out.println("restart this level ");
-                }
-                if (e.getKeyCode() == KeyEvent.VK_1 && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-                    System.out.println("restart");
-                }
-                if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-                    System.out.println("pause");
-                }
-                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-                    System.out.println("esc");
-                }
-                if (e.getKeyCode() == KeyEvent.VK_UP) {
-                    System.out.println("up");
-                }
-                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-                    System.out.println("down");
-                }
-                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-                    System.out.println("left");
-                }
-                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-                    System.out.println("right");
-                }
-
 
             }
         });
     }
 
     public void gameWindow(){
-        JFrame main = new JFrame();
-        setLayout(new BorderLayout());
-        setSize(800,800);
-        setVisible(true);
+
     }
 
     public void displayTime(){
