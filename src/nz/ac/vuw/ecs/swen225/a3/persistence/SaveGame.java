b356aaf7d40cd94f1edc70e64a834d9c64eec3dd
Matt <matt.rothwell.m@gmail.com>
2019-09-14 23:26:59 +1200

    Added methods to SaveGame

--- src/nz/ac/vuw/ecs/swen225/a3/persistence/SaveGame.java
+++ src/nz/ac/vuw/ecs/swen225/a3/persistence/SaveGame.java
@@ -1,4 +1,15 @@
 package nz.ac.vuw.ecs.swen225.a3.persistence;
 
+import nz.ac.vuw.ecs.swen225.a3.maze.Maze;
+
+/**
+ * This class contains methods relevent to saving of the current game state to a json file.
+ */
 public class SaveGame {
+
+    public static void saveGame(Maze maze){
+
+    }
+
+
 }
