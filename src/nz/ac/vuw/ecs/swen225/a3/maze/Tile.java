b4e4e05245a93bfc334052d3c0f2dcf6761b8552
munnetha <55314694+munnetha@users.noreply.github.com>
2019-09-14 22:52:30 +1200

    Testing Pushing with Tile Class
    Updated the tile class to give it a constructor and some variables. Basically testing that pushing and committing is working before I make any drastic changes.

--- src/nz/ac/vuw/ecs/swen225/a3/maze/Tile.java
+++ src/nz/ac/vuw/ecs/swen225/a3/maze/Tile.java
@@ -1,4 +1,14 @@
 package nz.ac.vuw.ecs.swen225.a3.maze;
 
 public class Tile {
+	
+	private Entity entity;
+	private boolean wall;
+	
+	public Tile(boolean wall) {
+		this.wall = wall;
+		entity = null;
+	}
+	
+	
 }
