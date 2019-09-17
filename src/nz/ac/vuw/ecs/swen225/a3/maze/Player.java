949a18fc70044879bf46831702f7d0e635a9342e
teagachu <teagachu@users.noreply.github.com>
2019-09-17 20:34:49 +1200

    Added functionality for players
    Added an inventory and an enum that can be used to move the player and get the direction they are facing.

--- src/nz/ac/vuw/ecs/swen225/a3/maze/Player.java
+++ src/nz/ac/vuw/ecs/swen225/a3/maze/Player.java
@@ -1,14 +1,38 @@
 package nz.ac.vuw.ecs.swen225.a3.maze;
 
+import java.util.List;
+
 
 public class Player extends Entity {
 	
-	public Player(int row, int col) {
-		super(row, col);
-		// TODO Auto-generated constructor stub
-	}
+
 	private int row, col;
 	private int totalMoves;
+	private Direction direction;
+	
+	private List<Entity> inventory;
+	
+	public Player(int row, int col) {
+		super(row, col);
+	}
+	
+	public void setInventory(List<Entity> inv) {
+		inventory = inv;
+	}
+	
+	public List<Entity> getInventory() {
+		return inventory;
+	}
+	
+	public Direction getDirection(){
+		return direction;
+	}
+	
+	public void setDirection(Direction dir){
+		direction = dir;
+	}
+	
+	
 	@Override
 	public void onTouch(Entity e) {
 		// TODO Auto-generated method stub
