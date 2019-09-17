0f996fa0579ed365fd52b709719d220565b0a749
teagachu <teagachu@users.noreply.github.com>
2019-09-17 19:53:23 +1200

    Fixed the player class
    Added a constructor and the unimplement methods so that the code compiles.

--- src/nz/ac/vuw/ecs/swen225/a3/maze/Player.java
+++ src/nz/ac/vuw/ecs/swen225/a3/maze/Player.java
@@ -2,12 +2,18 @@ package nz.ac.vuw.ecs.swen225.a3.maze;
 
 
 public class Player extends Entity {
+	
+	public Player(int row, int col) {
+		super(row, col);
+		// TODO Auto-generated constructor stub
+	}
 	private int row, col;
 	private int totalMoves;
+	@Override
+	public void onTouch(Entity e) {
+		// TODO Auto-generated method stub
+		e.onTouch(this);
+	}
 
-//	@Override
-//	public void onTouch(Entity e) {
-//		e.onTouch(this);
-//	}
 	
 }
