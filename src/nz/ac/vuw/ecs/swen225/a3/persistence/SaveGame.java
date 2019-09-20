565356699f2075cef3e4b65c7fb555615dec2a1b
Matt <matt.rothwell.m@gmail.com>
2019-09-20 13:38:08 +1200

    Added methods and classes
    The LoadGame class was created with some method stubs and the SaveGame class got some method stubs as well.

--- src/nz/ac/vuw/ecs/swen225/a3/persistence/SaveGame.java
+++ src/nz/ac/vuw/ecs/swen225/a3/persistence/SaveGame.java
@@ -3,11 +3,27 @@ package nz.ac.vuw.ecs.swen225.a3.persistence;
 import nz.ac.vuw.ecs.swen225.a3.maze.Maze;
 
 /**
- * This class contains methods relevent to saving of the current game state to a json file.
+ * This class contains methods relevant to saving of the current game state to a json file.
+ *
+ * @author Matt Rothwell
  */
 public class SaveGame {
 
+    /**
+     * Save the game state so that the player can resume next time the program is
+     * opened.
+     * @param maze the board.
+     */
     public static void saveGame(Maze maze){
+
+    }
+
+    /**
+     * The player has chosen to exit the game, without saving the current game state
+     * Therefore this method simply saves the number of the last unfinished level they were on
+     * so that the game can resume on this next time.
+     */
+    public static void saveLevel(){
 
     }
 
