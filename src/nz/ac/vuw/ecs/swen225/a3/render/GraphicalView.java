18c87afd56e82b603172538e6d77736328b17eb1
teagachu <teagachu@users.noreply.github.com>
2019-09-23 19:03:41 +1200

    Created Render Package
    I created the render package, and added class to draw the board and the image icons

---
+++ src/nz/ac/vuw/ecs/swen225/a3/render/GraphicalView.java
@@ -0,0 +1,112 @@
+package nz.ac.vuw.ecs.swen225.a3.render;
+
+import nz.ac.vuw.ecs.swen225.a3.maze.*;
+import javax.swing.*;
+
+public class GraphicalView {
+	private Render renderer;
+	private Maze maze;
+
+	public GraphicalView(Render r, Maze m) {
+		renderer = r;
+		maze = m;
+	}
+
+	/**
+	 * Used to create the board grid.
+	 *
+	 * @param i
+	 * x-coordinate value of tile we are checking.
+	 * @param j
+	 * y-coordinate value of tile we are checking.
+	 * @param m
+	 * The current maze.
+	 * @return
+	 * Returns the correct image icon for this tile.
+	 */
+	public ImageIcon getImageIcon(int i, int j, Maze m) {
+		Tile[][] tiles = m.getTiles();
+		switch(tiles[i][j].getType()) {
+		case FLOOR:
+			return null;
+		case WALL:
+			return null;
+		case HINT:
+			return null;
+		case GOAL:
+			return null;
+		default:
+			return null;
+		}
+	}
+
+	/**
+	 * Gets the key's icon.
+	 *
+	 * @param color
+	 * The color of the key.
+	 * @return
+	 * The image of the key.
+	 */
+	public ImageIcon getKeyIcon(BasicColor color) {
+		switch(color) {
+		case RED:
+			return null;
+		case GREEN:
+			return null;
+		case YELLOW:
+			return null;
+		case BLUE:
+			return null;
+		default:
+			return null;
+		}
+
+	}
+
+	/**
+	 * Gets the door's icon.
+	 *
+	 * @param color
+	 * The color of the door.
+	 * @return
+	 * The image of the door..
+	 */
+	public ImageIcon getDoorIcon(BasicColor color) {
+		switch(color) {
+		case RED:
+			return null;
+		case GREEN:
+			return null;
+		case YELLOW:
+			return null;
+		case BLUE:
+			return null;
+		default:
+			return null;
+		}
+
+	}
+
+
+	/**
+	 * A method that both resets all of the players and weapons on a grid, then draws
+	 * them in their correct position.
+	 *
+	 */
+	void drawOnGrid() {
+
+		for (int i = 0; i < maze.getTiles().length; i++) {
+			for (int j = 0; j <  maze.getTiles().length; j++) {
+
+				try {
+					renderer.getBoard()[i][j].remove(1);
+				}
+				catch(ArrayIndexOutOfBoundsException e) {
+
+				}
+
+			}
+		}
+	}
+}
---
+++ src/nz/ac/vuw/ecs/swen225/a3/render/Render.java
@@ -0,0 +1,53 @@
+package nz.ac.vuw.ecs.swen225.a3.render;
+
+import nz.ac.vuw.ecs.swen225.a3.maze.*;
+
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+import nz.ac.vuw.ecs.swen225.a3.application.*;
+import nz.ac.vuw.ecs.swen225.a3.application.ui.*;
+
+public class Render {
+	private Game game;
+	private GamePanel gamePanel;
+	private Maze maze;
+	private GraphicalView images;
+	private JLabel[][] board;
+
+	public Render(Game g, GamePanel gp, Maze m) {
+		images = new GraphicalView(this, m);
+		game = g;
+		gamePanel = gp;
+		maze = m;
+	}
+
+
+	public void createGrid() {
+		Tile[][] tiles = maze.getTiles();
+		board = new JLabel[tiles.length][tiles.length];
+
+		for(int i = 0; i<tiles.length; i++) {
+			for(int j = 0; j<tiles.length; j++) {
+
+				JLabel tile = new JLabel(images.getImageIcon(i,j,maze));
+				tile.setLayout(new BorderLayout());
+				board[i][j] = tile;
+			}
+		}
+	}
+
+	public void createBoard() {
+	}
+
+	/**
+	 *
+	 * @return
+	 * Returns the board as an array of image icons
+	 */
+	public JLabel[][] getBoard() {
+		return board;
+	}
+
+}
+
